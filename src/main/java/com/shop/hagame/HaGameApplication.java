d5d56d96eaddb63a8ebb37001b2cab30a0e641ac
huy1235588 <viettien4869@gmail.com>
2024-11-24 23:15:21 +0700

    Initial commit

---
+++ src/main/java/com/shop/hagame/HaGameApplication.java
@@ -0,0 +1,13 @@
+package com.shop.hagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaGameApplication.class, args);
+	}
+
+}
